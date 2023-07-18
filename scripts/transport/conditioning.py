5097f023b1730328d0219f2008550ec9d157fc4f
Alois Duston <alois.duston@parsely.com>
2023-07-18 16:38:19 -0700

    generic commit

--- scripts/transport/conditioning.py
+++ scripts/transport/conditioning.py
@@ -322,7 +322,7 @@ def conditional_transport_exp(ref_gen, target_gen, N, t_iter = 801, exp_name= 'e
 
     for i,csample in enumerate(slice_samples):
         csample = csample.T[0].T
-        plt.hist(csample.detach().cpu().numpy(), label = f'z = {slice_vals[i]}', bins = 40, range=[-1.5, 1.501])
+        plt.hist(csample.detach().cpu().numpy(), label = f'z = {slice_vals[i]}', bins = 40, range=[-1, 3])
     plt.legend()
     plt.savefig(f'{save_dir}/cond_hist.png')
     clear_plt()
@@ -330,11 +330,11 @@ def conditional_transport_exp(ref_gen, target_gen, N, t_iter = 801, exp_name= 'e
     target_sample = torch.concat([geq_1d(X_target), geq_1d(Y_target)], dim=1)
     sample = flip_2tensor(sample.detach())
 
-    sample_scatter(sample, f'{save_dir}/cond_sample.png', bins=25, d=2, range = [[-2.5,2.5],[-1,1]])
-    sample_hmap(sample, f'{save_dir}/cond_sample_map.png', bins=25, d=2, range = [[-2.5,2.5],[-1,1]])
+    sample_scatter(sample, f'{save_dir}/cond_sample.png', bins=25, d=2, range = [[-2.5,2.5],[-1,3]])
+    sample_hmap(sample, f'{save_dir}/cond_sample_map.png', bins=25, d=2, range = [[-2.5,2.5],[-1,3]])
 
-    sample_scatter(target_sample, f'{save_dir}/target_sample.png', bins=25, d=2, range = [[-2.5,2.5],[-1,1]])
-    sample_hmap(target_sample, f'{save_dir}/target_sample_map.png', bins=25, d=2, range = [[-2.5,2.5],[-1,1]])
+    sample_scatter(target_sample, f'{save_dir}/target_sample.png', bins=25, d=2, range = [[-2.5,2.5],[-1,3]])
+    sample_hmap(target_sample, f'{save_dir}/target_sample_map.png', bins=25, d=2, range = [[-2.5,2.5],[-1,3]])
 
 
   #scp -r ald6fd@klone.hyak.uw.edu:/mmfs1/gscratch/dynamicsai/ald6fd/measure_transport/data/kernel_transport/mgan23/ /Users/aloisduston/Desktop/Math/Research/Bambdad/Measure_transport/data/kernel_transport/
@@ -347,7 +347,7 @@ def run():
         device = 'cpu'
 
     ref_gen = sample_normal
-    target_gen = mgan2
+    target_gen = mgan1
 
     l = l_scale(torch.tensor(ref_gen(5000)[:, 1]))
     #mmd_params = {'name': 'radial', 'l': l/7, 'sigma': 1}
@@ -356,7 +356,7 @@ def run():
     fit_params = {'name': 'r_quadratic', 'l': l * torch.exp(torch.tensor(-1.25)), 'alpha': 1}
     exp_params = {'fit': mmd_params, 'mmd': fit_params}
 
-    conditional_transport_exp(ref_gen, target_gen, N= 5000, t_iter=2001, exp_name='mgan2_exp', params=exp_params)
+    conditional_transport_exp(ref_gen, target_gen, N= 5000, t_iter=2001, exp_name='mgan1_exp', params=exp_params)
 
 
     '''
