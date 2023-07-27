46881d018c7be04108d6e356c54e17c54f88be94
Alois Duston <alois.duston@parsely.com>
2023-07-26 20:06:53 -0700

    generic commit

--- scripts/transport/c_transport.py
+++ scripts/transport/c_transport.py
@@ -362,7 +362,7 @@ def run():
     slice_range = [-3,3]
     process_funcs = []
     #process_funcs = [flip_2tensor, flip_2tensor]
-    conditional_transport_exp(ref_gen, target_gen, exp_name= 'spiral_composed', N = 3000, n_iter = 5000,
+    conditional_transport_exp(ref_gen, target_gen, exp_name= 'spiral_composed', N = 2000, n_iter = 4000,
                               plt_range=range, slice_range= slice_range, process_funcs=process_funcs, slice_vals=[0])
 
 
