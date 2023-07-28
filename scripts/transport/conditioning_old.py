e35ec9e29ad5eb120e1fa0c72f8e2688cf384df9
Alois Duston <alois.duston@parsely.com>
2023-07-28 14:02:32 -0700

    generic commit

--- scripts/transport/conditioning_old.py
+++ scripts/transport/conditioning_old.py
@@ -16,6 +16,14 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+def geq_1d(tensor):
+    if not len(tensor.shape):
+        tensor = tensor.reshape(1,1)
+    elif len(tensor.shape) == 1:
+        tensor = tensor.reshape(len(tensor), 1)
+    return tensor
+
+
 def flip_2tensor(tensor):
     Ttensor = deepcopy(tensor.T)
     Ttensor[0] = deepcopy(tensor.T[1])
@@ -34,8 +42,7 @@ def mmd(Z, Y, mmd_kernel):
     return normalization * (torch.mean(k_ZZ)) - 2 * torch.mean(k_ZY) + k_YY_mean
 
 
-
-class TwoDCondTransportKernel(nn.Module):
+class CondTransportKernel(nn.Module):
     def __init__(self, base_params, device=None):
         super().__init__()
         if device:
@@ -46,8 +53,8 @@ class TwoDCondTransportKernel(nn.Module):
             else:
                 self.device = 'cpu'
         self.dtype = torch.float32
-        self.params = base_params
         base_params['device'] = self.device
+        self.params = base_params
 
         self.Z_ref= geq_1d(torch.tensor(base_params['Z_ref'], device=self.device, dtype=self.dtype))
         self.Y_ref = geq_1d(torch.tensor(base_params['Y_ref'], device = self.device, dtype = self.dtype))
@@ -84,7 +91,7 @@ class TwoDCondTransportKernel(nn.Module):
 
     def init_Z(self):
         return 0 * deepcopy(self.Y_target - self.Y_ref)
-        return torch.zeros(self.Y_ref.shape , device = self.device, dtype = self.dtype)
+        #return torch.zeros(self.Y_ref.shape , device = self.device, dtype = self.dtype)
 
 
     def init_alpha_x(self):
@@ -160,6 +167,8 @@ def U_KL(sample, unif_range = [-3,3]):
     densitys = np.asarray(densitys)
     KL_div = np.sum(np.log(1 / densitys))
     return KL_div
+
+
 
 
 
@@ -244,7 +253,7 @@ def light_conditional_transport_exp(ref_sample, target_sample, ref_test_sample, 
                                  'reg_lambda': 1e-5, 'print_freq':100, 'learning_rate': .06,
                                  'nugget': 1e-4, 'X_tilde': X_target, 'alpha_y': [], 'alpha_x': False}
 
-        cond_transport_kernel = TwoDCondTransportKernel(cond_transport_params)
+        cond_transport_kernel = CondTransportKernel(cond_transport_params)
         train_kernel(cond_transport_kernel, n_iter= 2 * t_iter)
         sample = cond_transport_kernel.map(X_target_test, Y_ref_test)
 
@@ -297,7 +306,7 @@ def conditional_transport_exp(ref_gen, target_gen, N, t_iter = 801,exp_name= 'ex
                         'reg_lambda':  1e-5, 'print_freq': 100, 'learning_rate': .06,
                         'nugget': 1e-4, 'X_tilde': X_target, 'alpha_y': [], 'alpha_x': False}
 
-    cond_transport_kernel = TwoDCondTransportKernel(cond_transport_params)
+    cond_transport_kernel = CondTransportKernel(cond_transport_params)
     train_kernel(cond_transport_kernel, n_iter= 4 * t_iter)
     sample = cond_transport_kernel.map(X_target, Y_ref)
 
@@ -350,8 +359,8 @@ def run():
     exp_params = {'fit': mmd_params, 'mmd': fit_params}
     range = [[-3,3], [-3,3]]
 
-    conditional_transport_exp(ref_gen, target_gen, N= 30, t_iter=100,
-                              exp_name='mgan_2', params=exp_params, plt_range=range)
+    conditional_transport_exp(ref_gen, target_gen, N= 3000, t_iter=2001,
+                              exp_name='ring_exp', params=exp_params, plt_range=range)
 
 
 if __name__=='__main__':
