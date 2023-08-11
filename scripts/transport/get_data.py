154329902839237bfaedbcb7d15ad4cfe4b771df
Alois Duston <alois.duston@parsely.com>
2023-08-10 23:12:52 -0700

    generic commit

--- scripts/transport/get_data.py
+++ scripts/transport/get_data.py
@@ -484,7 +484,7 @@ def sample_x_torus(N, x = 0, eps_scale = .01, eps = .01):
     return slice_sample[:N]
 
 def sample_spheres_prior(N):
-    R = sample_uniform(N, d=1, l=.65, h=1.35)
+    R = sample_uniform(N, d=1, l=.75, h=1.25)
     X = np.asarray([sample_uniform(1, d=1, l=(.9 * -r), h=(.9*r))[0] for r in R])
     return np.concatenate([R,X], axis = 1)
 
