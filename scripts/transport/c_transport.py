7130232dfcd480efd351d555c90aca6cec452b4c
Alois Duston <alois.duston@parsely.com>
2023-08-14 15:07:58 -0700

    generic commit

--- scripts/transport/c_transport.py
+++ scripts/transport/c_transport.py
@@ -625,9 +625,10 @@ def vl_exp(N=10000, n_iter=31, Yd=18, normal=True, exp_name='vl_exp'):
 def run():
     ref_gen = sample_normal
     target_gen = sample_swiss_roll
-    N = 500
+    N = 1500
     two_d_exp(ref_gen, target_gen, N, n_iter=101, plt_range=[[-3, 3], [-3, 3]], process_funcs=[], skip_idx=1,
-              slice_vals=[], slice_range=[-1.5, 1.5], exp_name='swiss_kflow', vmax=.25, n_transports=20)
+              slice_vals=[], slice_range=[-1.5, 1.5], exp_name='swiss_kflow', n_transports=20)
+
 
 if __name__=='__main__':
     run()
