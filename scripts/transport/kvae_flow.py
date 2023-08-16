b740b44af78465ad6e9f2150f661abfd77252f52
Alois Duston <alois.duston@parsely.com>
2023-08-16 10:11:06 -0700

    generic commit

--- scripts/transport/kvae_flow.py
+++ scripts/transport/kvae_flow.py
@@ -653,18 +653,16 @@ def vl_exp(N=10000, n_iter=51, Yd=18, normal=True, exp_name='kvl_exp2', n_transp
                                                          plot_idx= [], plt_range = None ,idx_dict= idx_dict,
                                                          n_transports = n_transports, batch_size = batch_size)
 
-    target_sample = get_VL_data(N_plot, normal=False, Yd = Yd)[4:]
+    target_sample = get_VL_data(N_plot, normal=False, Yd = Yd)
     mu = np.mean(target_sample, axis = 0)
     sigma = np.std(target_sample, axis = 0)
 
     slice_val = np.asarray([.8, .041, 1.07, .04])
     X = np.full((N_plot, 4), slice_val)
     ref_slice_sample = get_VL_data(10 * N_plot, X=X, Yd=Yd, normal = False,  T = 20)
-    ref_slice_sample[4:] -= mu
-    ref_slice_sample[4:] /= sigma
 
-    ref_slice_sample[:4] -= X_mean
-    ref_slice_sample[:4] /= X_std
+    ref_slice_sample -= mu
+    ref_slice_sample /= sigma
 
     ref_sample = ref_gen(N_plot)
 
@@ -720,7 +718,7 @@ def run():
     #two_d_exp(ref_gen, sample_spirals, N=N, n_iter=49, plt_range=[[-3, 3], [-3, 3]], process_funcs=[],
               #skip_idx=1, slice_vals=[-1,0,1], slice_range=[-3,3], exp_name='exp', n_transports=200, vmax=.15,
               #batch_size = batch_size, reg_lambda= 1e-5, N_plot = 10000)
-    vl_exp(batch_size=9000, n_transports=150)
+    vl_exp(batch_size=9000, n_transports=200)
 
     #vl_exp(N = 20000, batch_size=4000, n_transports=150, n_iter=51, exp_name='kvl_exp')
     #0.0008
