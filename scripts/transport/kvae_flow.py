aa3be7a4a4f10d9134642dff9d6b87bcf6172d08
Alois Duston <alois.duston@parsely.com>
2023-08-16 12:45:22 -0700

    generic commit

--- scripts/transport/kvae_flow.py
+++ scripts/transport/kvae_flow.py
@@ -759,7 +759,7 @@ def run():
     print('\n')
     print('Eps = .0001 exps')
     for i in range(3):
-        two_d_exp(ref_gen, sample_swiss_roll, N=N, n_iter=49, plt_range=[[-3, 3], [-3, 3]], process_funcs=[],
+        two_d_exp(ref_gen, sample_spirals, N=N, n_iter=49, plt_range=[[-3, 3], [-3, 3]], process_funcs=[],
                   skip_idx=1, slice_vals=[0], slice_range=[-3, 3], exp_name='exp4', n_transports=n_transports, vmax=.25,
                   batch_size=batch_size, reg_lambda=1e-5, N_plot=4000, final_eps=.0001)
 
