1e7b2698b18d19760328b8d82bf9464203f11093
Alois Duston <alois.duston@parsely.com>
2023-08-16 11:51:28 -0700

    generic commit

--- scripts/transport/kvae_flow.py
+++ scripts/transport/kvae_flow.py
@@ -738,21 +738,21 @@ def run():
     for i in range(3):
         two_d_exp(ref_gen, sample_swiss_roll, N=N, n_iter=49, plt_range=[[-3, 3], [-3, 3]], process_funcs=[],
                   skip_idx=1, slice_vals=[0], slice_range=[-3, 3], exp_name='exp2', n_transports=40, vmax=.25,
-                  batch_size=batch_size, reg_lambda=1e-5, N_plot=i * 700, final_eps=.01)
+                  batch_size=batch_size, reg_lambda=1e-5, N_plot=(i+1) * 700, final_eps=.01)
 
     print('\n \n')
     print('Eps = .001 exps')
     for i in range(3):
         two_d_exp(ref_gen, sample_swiss_roll, N=N, n_iter=49, plt_range=[[-3, 3], [-3, 3]], process_funcs=[],
                   skip_idx=1, slice_vals=[0], slice_range=[-3, 3], exp_name='exp3', n_transports=40, vmax=.25,
-                  batch_size=batch_size, reg_lambda=1e-5, N_plot=i * 700, final_eps=.001)
+                  batch_size=batch_size, reg_lambda=1e-5, N_plot=(i+1) * 700, final_eps=.001)
 
     print('\n \n')
     print('Eps = .0001 exps')
     for i in range(3):
         two_d_exp(ref_gen, sample_swiss_roll, N=N, n_iter=49, plt_range=[[-3, 3], [-3, 3]], process_funcs=[],
                   skip_idx=1, slice_vals=[0], slice_range=[-3, 3], exp_name='exp4', n_transports=40, vmax=.25,
-                  batch_size=batch_size, reg_lambda=1e-5, N_plot=i * 700, final_eps=.0001)
+                  batch_size=batch_size, reg_lambda=1e-5, N_plot=(i+1) * 700, final_eps=.0001)
 
     #vl_exp(batch_size=4500, n_transports=125)
 
