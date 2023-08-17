406185829beb184ed1e739f7536deeb33b8b3658
Alois Duston <alois.duston@parsely.com>
2023-08-17 14:17:13 -0700

    generic commit

--- scripts/transport/kvae_flow.py
+++ scripts/transport/kvae_flow.py
@@ -439,8 +439,8 @@ def comp_cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_eta_test, X_mu_test, Y_mu_te
 
         approx = True
         E_mmd_yy = model.E_mmd_YY
-        #if model.iters - iters < 2:
-            #return Comp_transport_model(model_params)
+        if model.iters - iters == 1:
+            return Comp_transport_model(model_params)
         iters = model.iters
 
     return Comp_transport_model(model_params)
