0fd72c22075c255ec0710803da0d84029dcc6ce0
Alois Duston <alois.duston@parsely.com>
2023-08-16 20:57:58 -0700

    generic commit

--- scripts/transport/kvae_flow2.py
+++ scripts/transport/kvae_flow2.py
@@ -727,7 +727,7 @@ def vl_exp(N=10000, n_iter=49, Yd=18, normal=True, exp_name='kvl_exp', n_transpo
 
 def run():
     vl_exp(n_iter=49, N=9000, batch_size=9000, n_transports=200, exp_name='kvl_exp_real2',
-           reg_lambda= 5e-6)
+           reg_lambda= 1e-6)
 
 
 if __name__=='__main__':
