5f1d9aeae09c37272b767bcd9f01872dc7b4ac5b
Alois Duston <alois.duston@parsely.com>
2023-08-17 15:02:45 -0700

    generic commit

--- scripts/transport/c_transport.py
+++ scripts/transport/c_transport.py
@@ -331,7 +331,7 @@ def cond_kernel_transport(X_mu, Y_mu, Y_eta, params, Y_approx = [], iters = 0,mm
                           Y_eta_test = [], X_mu_test = [],Y_mu_test = [], Y_approx_test = []):
     transport_params = {'X_mu': X_mu, 'Y_mu': Y_mu, 'Y_eta': Y_eta, 'reg_lambda': 5e-7, 'Y_approx': Y_approx,
                         'fit_kernel_params': deepcopy(params['mmd']), 'mmd_kernel_params': deepcopy(params['fit']),
-                        'print_freq': 50, 'learning_rate': .001, 'nugget': 1e-5, 'Y_eta_test': Y_eta_test,
+                        'print_freq': 5, 'learning_rate': .001, 'nugget': 1e-5, 'Y_eta_test': Y_eta_test,
                         'X_mu_test': X_mu_test, 'Y_mu_test': Y_mu_test, 'Y_approx_test': Y_approx_test,
                         'iters': iters, 'grad_cutoff': .005 ,'mmd_lambda': mmd_lambda}
     ctransport_kernel = CondTransportKernel(transport_params)
