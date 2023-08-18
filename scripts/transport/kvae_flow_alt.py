2ac06f9b883713b44b30e287d54a9b3bb2b537f4
Alois Duston <alois.duston@parsely.com>
2023-08-17 21:56:17 -0700

    generic commit

--- scripts/transport/kvae_flow_alt.py
+++ scripts/transport/kvae_flow_alt.py
@@ -413,7 +413,7 @@ def comp_cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_eta_test, X_mu_test, Y_mu_te
     approx = False
     mmd_lambda = 0
     E_mmd_yy = 0
-    grad_cutoff = .001
+    grad_cutoff = .0001
 
     target_eps = 1
 
