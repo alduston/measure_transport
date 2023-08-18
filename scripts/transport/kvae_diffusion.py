b99ed26f787316603acf1b71b844ba803b22d724
Alois Duston <alois.duston@parsely.com>
2023-08-18 11:58:13 -0700

    generic commit

--- scripts/transport/kvae_diffusion.py
+++ scripts/transport/kvae_diffusion.py
@@ -733,9 +733,9 @@ def vl_exp(N=4000, Yd=18, normal=True, exp_name='kvl_exp', n_transports=100,  N_
 
 
 def run():
-    two_d_exp(ref_gen=sample_normal, target_gen=sample_elden_ring, N=5000, exp_name='spiral_exp_alt4', n_transports=50,
-             slice_vals=[], plt_range=[[-3, 3], [-3, 3]], slice_range=[-1, 1], vmax=.15, skip_idx=1, N_plot=5000
-             ,plot_steps = True)
+    two_d_exp(ref_gen=sample_normal, target_gen=sample_elden_ring, N=5000, exp_name='elden_movie2', n_transports=60,
+             slice_vals=[], plt_range=[[-1, 1], [-1, 1]], slice_range=[-1, 1], vmax=6, skip_idx=1, N_plot=5000,
+             plot_steps = True)
 
 
 
