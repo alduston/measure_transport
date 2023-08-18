68bdd24df826dc5cad6a1ea8d5fda9da0f572968
Alois Duston <alois.duston@parsely.com>
2023-08-17 20:14:04 -0700

    generic commit

--- scripts/transport/kvae_flow_alt.py
+++ scripts/transport/kvae_flow_alt.py
@@ -385,7 +385,7 @@ class CondTransportKernel(nn.Module):
 
 def cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_mean, Y_var, X_mu_test, Y_eta_test, Y_mu_test,
                           Y_mean_test, Y_var_test, params, iters=-1, approx=False,mmd_lambda=0,
-                          reg_lambda=1e-5, E_mmd_yy=0, grad_cutoff = .01,n_iter = 49, target_eps = 1):
+                          reg_lambda=1e-5, E_mmd_yy=0, grad_cutoff = .01,n_iter = 101, target_eps = 1):
     transport_params = {'X_mu': X_mu, 'Y_mu': Y_mu, 'Y_eta': Y_eta, 'nugget': 1e-5, 'Y_var': Y_var, 'Y_mean': Y_mean,
                         'fit_kernel_params': deepcopy(params['fit']), 'mmd_kernel_params': deepcopy(params['mmd']),
                         'print_freq': 10, 'learning_rate': .001, 'reg_lambda': 1e-6,#reg_lambda,
@@ -411,7 +411,7 @@ def comp_cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_eta_test, X_mu_test, Y_mu_te
     approx = False
     mmd_lambda = 0
     E_mmd_yy = 0
-    grad_cutoff = .0007
+    grad_cutoff = .005
 
     target_eps = 1
 
