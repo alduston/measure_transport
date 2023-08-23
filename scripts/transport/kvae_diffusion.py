a549228dc53be896e3b3bdae5a9e9d08f3b2ff64
Alois Duston <alois.duston@parsely.com>
2023-08-23 14:05:12 -0700

    generic commit

--- scripts/transport/kvae_diffusion.py
+++ scripts/transport/kvae_diffusion.py
@@ -842,7 +842,7 @@ def run():
    target_gen = mgan2
    two_d_exp(ref_gen=sample_normal, target_gen = target_gen, N=4000, exp_name='mgan2_movie2', n_transports=60,
               slice_vals=[-1,0,1], plt_range=[[-2.5,2.5],[-1.05,1.05]], slice_range=[-1.5, 1.5], vmax=8, skip_idx=1,
-              N_plot=4000, plot_steps = True, normal = True, bins=100, var_eps = 1)
+              N_plot=4000, plot_steps = True, normal = True, bins=100, var_eps = .4)
 
    target_gen = sample_elden_ring
    two_d_exp(ref_gen=sample_normal, target_gen = target_gen, N=10000, exp_name='elden_movie', n_transports=60,
