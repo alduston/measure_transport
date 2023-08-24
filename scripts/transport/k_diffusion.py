38c1c07117e1bd4a5254f03951f116a5a5e46f77
Alois Duston <alois.duston@parsely.com>
2023-08-23 23:30:45 -0700

    generic commit

--- scripts/transport/k_diffusion.py
+++ scripts/transport/k_diffusion.py
@@ -852,10 +852,10 @@ def run():
               #slice_vals=[], plt_range=[[-4.1,4.1],[-4.1,4.1]], slice_range=[-1.5, 1.5], vmax=.12, skip_idx=1,
               #N_plot=10000, plot_steps = True, normal = True, bins=100, var_eps = .1)
 
-   target_gen = sample_spirals
+   target_gen = sample_elden_ring
    two_d_exp(ref_gen=sample_normal, target_gen = target_gen, N = 5000, exp_name='exp', n_transports=60,
-        slice_vals=[0], plt_range=[[-3,3],[-3,3]], slice_range=[-3, 3], vmax=.3, skip_idx=1,
-        N_plot= 5000, plot_steps = True, normal = True, bins=100, var_eps = 1/3)
+        slice_vals=[], plt_range=[[-1,1],[-1.05,1.05]], slice_range=[-3, 3], vmax=.3, skip_idx=1,
+        N_plot= 5000, plot_steps = True, normal = True, bins=100, var_eps = .1)
 
 #Test mmd :0.0021, Base mmd: 0.0105, NTest mmd :0.2039
 if __name__ == '__main__':
