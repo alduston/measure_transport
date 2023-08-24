b4dc961c1a49cd1fea8a5305d7158f0eb78dab53
Alois Duston <alois.duston@parsely.com>
2023-08-24 16:19:54 -0700

    generic commit

--- scripts/transport/kvae_diffusion.py
+++ scripts/transport/kvae_diffusion.py
@@ -436,7 +436,7 @@ class CondTransportKernel(nn.Module):
 
 def cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_mean, Y_var, X_mu_test, Y_eta_test, Y_mu_test, X_mu_val,
                           Y_mean_test, Y_var_test, params, iters=-1, approx=False,mmd_lambda=0, step_num = 1,
-                          reg_lambda=4e-7, grad_cutoff = .0001, n_iter = 200, target_eps = 1, var_eps = .1):
+                          reg_lambda=1e-6, grad_cutoff = .0001, n_iter = 125, target_eps = 1, var_eps = .1):
     transport_params = {'X_mu': X_mu, 'Y_mu': Y_mu, 'Y_eta': Y_eta, 'nugget': 1e-4, 'Y_var': Y_var, 'Y_mean': Y_mean,
                         'fit_kernel_params': deepcopy(params['fit']), 'mmd_kernel_params': deepcopy(params['mmd']),
                         'print_freq': 25, 'learning_rate': .001, 'reg_lambda': reg_lambda, 'var_eps': var_eps,
@@ -450,7 +450,7 @@ def cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_mean, Y_var, X_mu_test, Y_eta_tes
 
 
 def comp_cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_eta_test, X_mu_test, Y_mu_test, X_mu_val, params,
-                               final_eps=1e-2, n_transports=50, reg_lambda=4e-7, n_iter = 200,var_eps = .1):
+                               final_eps=1e-2, n_transports=50, reg_lambda=1e-6, n_iter = 125,var_eps = .1):
     param_keys = ['fit_kernel','Lambda_mean', 'X_mean',  'Lambda_var', 'X_var', 'var_eps']
     models_param_dict = {key: [] for key in param_keys}
     iters = 0
@@ -514,8 +514,8 @@ def zero_pad(array):
     return np.concatenate([zero_array, array], axis=1)
 
 
-def train_cond_transport(ref_gen, target_gen, params, N = 4000,  process_funcs=[],final_eps=1e-2,var_eps = .1,
-                         cond_model_trainer=cond_kernel_transport, idx_dict={}, reg_lambda=4e-7, n_transports=100):
+def train_cond_transport(ref_gen, target_gen, params, N = 4000,  process_funcs=[],final_eps=1e-3,var_eps = .1,
+                         cond_model_trainer=cond_kernel_transport, idx_dict={}, reg_lambda=1e-6, n_transports=100):
     ref_sample = ref_gen(N)
     target_sample = target_gen(N)
 
@@ -583,7 +583,7 @@ def compositional_gen(trained_models, ref_sample, target_sample, idx_dict, plot_
 def conditional_transport_exp(ref_gen, target_gen, N=4000, vmax=None, exp_name='exp', plt_range=None, bins=70,
                               process_funcs=[], N_plot=0, cond_model_trainer=comp_cond_kernel_transport,
                               final_eps=1e-2, skip_idx=0, plot_idx=[], n_transports=50, idx_dict={},
-                              plot_steps = False, reg_lambda = 4e-7, mu = 0, sigma = 1,var_eps = .1):
+                              plot_steps = False, reg_lambda = 7e-7, mu = 0, sigma = 1,var_eps = .1):
     save_dir = f'../../data/kernel_transport/{exp_name}'
     try:
         os.mkdir(save_dir)
@@ -662,7 +662,7 @@ def conditional_transport_exp(ref_gen, target_gen, N=4000, vmax=None, exp_name='
 
 def two_d_exp(ref_gen, target_gen, N=4000, plt_range=None, process_funcs=[], normal = True,
               slice_range=None, N_plot=4000, slice_vals=[], bins=70, exp_name='exp', skip_idx=1,
-              vmax=None, n_transports=60, reg_lambda=4e-7, final_eps=1e-2, plot_steps = False, var_eps = 1/3):
+              vmax=None, n_transports=60, reg_lambda=7e-7, final_eps=1e-3, plot_steps = False, var_eps = 1/3):
     save_dir = f'../../data/kernel_transport/{exp_name}'
     try:
         os.mkdir(save_dir)
@@ -843,7 +843,7 @@ def vl_exp(N=4000, Yd=18, normal=True, exp_name='kvl_exp', n_transports=60,  N_p
 
 def run():
     #arget_gen = sample_swiss_roll()
-    two_d_exp(ref_gen=sample_normal, target_gen=sample_elden_ring , N=7000, exp_name='exp', n_transports=40,
+    two_d_exp(ref_gen=sample_normal, target_gen=sample_elden_ring , N=7000, exp_name='exp', n_transports=60,
               slice_vals=[], plt_range=[[-1,1], [-1.05, 1.05]], slice_range=[-1.5, 1.5], vmax=8,
               skip_idx=1, N_plot=7000, plot_steps=False, normal=True, bins=100, var_eps=1/9)
 
