861b386ff33dc3348ef22e766495199307677dfe
Alois Duston <alois.duston@parsely.com>
2023-08-24 15:45:53 -0700

    generic commit

--- scripts/transport/kvae_diffusion.py
+++ scripts/transport/kvae_diffusion.py
@@ -489,7 +489,8 @@ def comp_cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_eta_test, X_mu_test, Y_mu_te
         approx = True
 
         iters = model.iters
-        target_eps *= noise_shrink_c
+        if not i%2:
+            target_eps *= noise_shrink_c
 
         if n_transports - i < 10:
             target_eps = 0
@@ -843,7 +844,7 @@ def vl_exp(N=4000, Yd=18, normal=True, exp_name='kvl_exp', n_transports=60,  N_p
 
 def run():
     #arget_gen = sample_swiss_roll()
-    two_d_exp(ref_gen=sample_normal, target_gen=sample_elden_ring, N=7000, exp_name='exp', n_transports=40,
+    two_d_exp(ref_gen=sample_normal, target_gen=sample_elden_ring, N=7000, exp_name='exp', n_transports=80,
               slice_vals=[], plt_range=[[-1,1], [-1.05, 1.05]], slice_range=[-1.5, 1.5], vmax=8,
               skip_idx=1, N_plot=5000, plot_steps=False, normal=True, bins=100, var_eps=1/9)
 
