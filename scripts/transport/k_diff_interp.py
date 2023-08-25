dcb8be51cd070081c895e5f3cfe2702776c2e8f4
Alois Duston <alois.duston@parsely.com>
2023-08-25 14:10:21 -0700

    generic commit

--- scripts/transport/k_diff_interp.py
+++ scripts/transport/k_diff_interp.py
@@ -851,10 +851,11 @@ def vl_exp(N=4000, Yd=18, normal=True, exp_name='kvl_exp', n_transports=60,  N_p
 
 
 def run():
-    two_d_exp(ref_gen=sample_normal, target_gen=sample_elden_ring , N=10000, exp_name='elden_movie2', n_transports=90,
+    two_d_exp(ref_gen=sample_normal, target_gen=sample_elden_ring , N=10000, exp_name='exp', n_transports=90,
               slice_vals=[], plt_range=[[-1,1], [-1.05, 1.05]], slice_range=[-1.5, 1.5], vmax=8,
-              skip_idx=1, N_plot=10000, plot_steps=True, normal=True, bins=100, var_eps=1/10)
+              skip_idx=1, N_plot=20000, plot_steps=True, normal=True, bins=100, var_eps=1/10)
 
+    '''
     two_d_exp(ref_gen=sample_normal, target_gen= sample_spirals , N=10000, exp_name='spiral_movie2', n_transports=90,
               slice_vals=[0], plt_range=[[-3,3], [-3, 3]], slice_range=[-3, 3], vmax=.32,
               skip_idx=1, N_plot=10000, plot_steps=True, normal=True, bins=100, var_eps=1/3)
@@ -869,6 +870,7 @@ def run():
 
     spheres_exp(9000, exp_name='spheres_exp2', n_transports=90)
     vl_exp(9000, exp_name='vl_exp2', n_transports=90)
+    '''
 
 if __name__ == '__main__':
     run()
