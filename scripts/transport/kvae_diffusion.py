0fe08191f91ab6a164b906e3f095ee7650b6f7d2
Alois Duston <alois.duston@parsely.com>
2023-08-25 16:54:15 -0700

    generic commit

--- scripts/transport/kvae_diffusion.py
+++ scripts/transport/kvae_diffusion.py
@@ -491,7 +491,6 @@ def comp_cond_kernel_transport(X_mu, Y_mu, Y_eta, Y_eta_test, X_mu_test, Y_mu_te
 
         iters = model.iters
         print(f'Target eps = {target_eps}')
-        eps = 1-noise_shrink_c
         noise_eps = ((i+1) * eps)/(1 + (i * eps))
         target_eps = 1 - noise_eps
 
