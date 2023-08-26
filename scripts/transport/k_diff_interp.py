5167ac053165c4529623416bf52258b622ccc5d7
Alois Duston <alois.duston@parsely.com>
2023-08-26 13:27:33 -0700

    generic commit

--- scripts/transport/k_diff_interp.py
+++ scripts/transport/k_diff_interp.py
@@ -839,7 +839,7 @@ def run():
               n_transports=100, slice_vals=[-1,0,1], plt_range=[[-4.4, 4.4], [-4.1, 4.1]], slice_range=[-4.4, 4.4], vmax=.12,
               skip_idx=1, N_plot=10000, plot_steps=True, normal=True, bins=100, var_eps= 1/3)
     
-
+    '''
     vl_exp(9000, exp_name='lv_exp_alt', n_transports=100)
     spheres_exp(9000, exp_name='spheres_exp_alt', n_transports=100, normal = False)
     '''
