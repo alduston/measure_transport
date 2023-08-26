a51b7853e0932767bea4026300f19b07f9d665b8
Alois Duston <alois.duston@parsely.com>
2023-08-26 13:25:36 -0700

    generic commit

--- scripts/transport/k_diff_interp.py
+++ scripts/transport/k_diff_interp.py
@@ -662,8 +662,10 @@ def two_d_exp(ref_gen, target_gen, N=4000, plt_range=None, process_funcs=[], nor
     save_dir = f'../../data/transport/{exp_name}'
     try:
         os.mkdir(save_dir)
+        print('here')
     except OSError:
         pass
+
 
     mu, sigma = 0, 1
     if normal:
@@ -821,6 +823,10 @@ def vl_exp(N=10000, Yd=18, normal=True, exp_name='kvl_exp', n_transports=100,  N
 
 
 def run():
+    two_d_exp(ref_gen=sample_normal, target_gen=mgan1, N=100, exp_name='mgan1_movie_alt', n_transports=5,
+              slice_vals=[-1, 0, 1], plt_range=[[-2.5, 2.5], [-1, 3]], slice_range=[-1.5, 1.5], vmax=1.2,
+              skip_idx=1, N_plot=100, plot_steps=True, normal=True, bins=100, var_eps=1 / 3)
+
     '''
     two_d_exp(ref_gen=sample_normal, target_gen=mgan1, N=10000, exp_name='mgan1_movie_alt', n_transports=100,
              slice_vals=[-1, 0, 1], plt_range=[[-2.5, 2.5], [-1, 3]], slice_range=[-1.5, 1.5], vmax=1.2,
@@ -837,10 +843,11 @@ def run():
     two_d_exp(ref_gen=sample_normal, target_gen=sample_checkerboard, N=10000, exp_name='checkerboard_movie_alt',
               n_transports=100, slice_vals=[-1,0,1], plt_range=[[-4.4, 4.4], [-4.1, 4.1]], slice_range=[-4.4, 4.4], vmax=.12,
               skip_idx=1, N_plot=10000, plot_steps=True, normal=True, bins=100, var_eps= 1/3)
-    '''
+    
 
     vl_exp(9000, exp_name='lv_exp_alt', n_transports=100)
     spheres_exp(9000, exp_name='spheres_exp_alt', n_transports=100, normal = False)
+    '''
 
 
 if __name__ == '__main__':
