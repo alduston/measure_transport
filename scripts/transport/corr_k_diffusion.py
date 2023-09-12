4b146d8da2821a56990dd9fbf25cc1a7b0dc833c
Alois Duston <alois.duston@parsely.com>
2023-09-12 13:13:07 -0700

    generic commit

--- scripts/transport/corr_k_diffusion.py
+++ scripts/transport/corr_k_diffusion.py
@@ -956,7 +956,8 @@ def test_panel(plot_steps = False, approx_path = False, N = 10000, test_name = '
                     pass
 
 def run():
-    test_panel(N=5000, n_transports=100, k=10, approx_path=False, test_name='test1', test_keys=['elden', 'mgan1'])
+    test_panel(N=5000, n_transports=100, k=2, approx_path=False, test_name='test1', test_keys=['elden', 'mgan1',
+                                                                                                'spiral', 'checker'])
 
     #test_panel(N=2500, n_transports=100 , k=1, approx_path=False, test_name='exp', test_keys=['spheres'] )
     #test_panel(N=5000, n_transports=60, k=10, approx_path=False, test_name='lv_test_med2', test_keys=['lv'])
