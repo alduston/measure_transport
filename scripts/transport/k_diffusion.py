046a2c22d1d94b614d994773e36b0f5c47acef02
Alois Duston <alois.duston@parsely.com>
2023-09-12 17:10:47 -0700

    generic commit

--- scripts/transport/k_diffusion.py
+++ scripts/transport/k_diffusion.py
@@ -306,8 +306,9 @@ class CondTransportKernel(nn.Module):
         self.mmd_lambda = 1
         self.mmd_lambda = (1 / self.loss_mmd().detach())
 
-        self.mmd_lambda_inv = 1
-        self.mmd_lambda_inv =  (1 / self.loss_inv().detach())
+        self.mmd_lambda_inv = 0
+        if self.approx:
+            self.mmd_lambda_inv =  (1 / self.loss_inv().detach())
 
         self.reg_lambda = self.params['reg_lambda'] * self.mmd_lambda
 
@@ -451,7 +452,7 @@ class CondTransportKernel(nn.Module):
 
         target = torch.concat([self.X_mu, Y_input], dim=1)
 
-        noised_Y_approx = self.invert_denoising(Y_approx, self.Y_eta_flip)
+        noised_Y_approx = self.invert_denoising(Y_approx, self.Y_eta)
         noised_map_vec = torch.concat([self.X_mu, noised_Y_approx], dim=1)
 
         mmd_ZZ = self.mmd_kernel(noised_map_vec, noised_map_vec)
@@ -464,6 +465,7 @@ class CondTransportKernel(nn.Module):
         Ek_ZY = alpha @ mmd_ZY @ alpha
         Ek_YY = alpha @ mmd_YY @ alpha
         mmd = Ek_ZZ - (2 * Ek_ZY) + Ek_YY
+        print(mmd)
 
         return mmd * self.mmd_lambda_inv
 
@@ -988,7 +990,7 @@ def test_panel(plot_steps = False, approx_path = False, N = 10000, test_name = '
                     pass
 
 def run():
-    test_panel(N=3000, n_transports=100, k=1, approx_path=False, test_name='test3', test_keys=['lv'])
+    test_panel(N=1500, n_transports=100, k=1, approx_path=False, test_name='test3', test_keys=['spiral'])
 
     #test_panel(N=2500, n_transports=100 , k=1, approx_path=False, test_name='exp', test_keys=['spheres'])
     #test_panel(N=5000, n_transports=60, k=10, approx_path=False, test_name='lv_test_med2', test_keys=['lv'])
